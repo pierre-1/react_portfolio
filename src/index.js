9bad5c9d90a9d72ee5a00a73b4b61694fc927c3a
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-17 23:00:34 +0100

    adds intiial CSS and Route setup for Portfolio

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Hello from "./Hello";
+import Header from "./Header";
+import Footer from "./Footer";
+
+import { BrowserRouter } from "react-router-dom";
+import Projects from "./Projects";
+import About from "./About";
 
 const App = () => {
-  return <Hello />;
+  return (
+  <div>
+      <Header />
+      <Hello />
+      <Footer />
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>, 
+document.getElementById("app")
+);
